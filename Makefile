91df27af87702ec4519dc9d11ab165d296866585
Daniel Kowalski <d.kowalski@onet.pl>
2016-06-14 21:06:03 +0200

    Add some slides, some fixes
    Add some slide, make Makefile to build presentation, add signals
    calling and canceling played tracks to audio.py.

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 PICS=service_full.png bus_clients.png daemon_with_clients.png
 
-all: $(PICS)
+all: presentation.tex $(PICS)
+	pdflatex presentation.tex
+	# and second time for "tableofcontents"
+	pdflatex presentation.tex
 
-clean:
-	rm *.png
-
-%.png: %.ditaa $(DEPS)
+%.png: %.ditaa
 	ditaa -E $< $@
 
+clean:
+	rm -f *.png
+	find . -iname 'presentation.*' -and -not -iname 'presentation.tex' -delete
--- audio.py
+++ audio.py
@@ -9,7 +9,7 @@ import dbus
 import dbus.service
 from dbus.mainloop.glib import DBusGMainLoop
 
-bus_name = "com.harman.audio"
+bus_name = "com.example.audio"
 
 class Player(dbus.service.Object):
     interface = bus_name + ".Player"
@@ -18,12 +18,13 @@ class Player(dbus.service.Object):
         dbus.service.Object.__init__(self, bus, self.path)
         self.tracks = []
         self.now_playing = None
+        self.now_playing_source = None
 
     @dbus.service.method(dbus_interface=interface,
                          in_signature='si')
     def AddTrack(self, name, time):
         self.tracks.append((name, time))
-        print "Added track to play: '%s' [%sd]"%(name, time)
+        print "[%s] Added track to play: '%s' [%sd]"%(self.path, name, time)
     
     @dbus.service.method(dbus_interface=interface,
                          out_signature='a(si)')
@@ -36,7 +37,7 @@ class Player(dbus.service.Object):
         if index >=0 and index < len(self.tracks):
             self.PlayTrack(index)
         else:
-            raise dbus.exceptions.DBusException("There is no track with index: %d"%index, name="com.harman.audio.NoTrack")
+            raise dbus.exceptions.DBusException("There is no track with index: %d"%index, name="com.example.audio.NoTrack")
     
     @dbus.service.signal(dbus_interface=interface,
                          signature='si')
@@ -51,17 +52,22 @@ class Player(dbus.service.Object):
     def PlayTrack(self, index):
         name, time = self.tracks[index]
         self.now_playing = index 
-        print "Now playing: %s [%ds]"%(name, time)      
-        gobject.timeout_add_seconds(time, self.PlayedTrack, self.now_playing)
+        print "[%s] Now playing: '%s' [%ds]"%(self.path, name, time)
+        self.NowPlaying(name, time)
+        if (self.now_playing_source):
+            gobject.source_remove(self.now_playing_source)
+        self.now_playing_source = \
+            gobject.timeout_add_seconds(time, self.PlayedTrack, self.now_playing)
     
     def PlayedTrack(self, index):
         name, time = self.tracks[index]
-        print "Played: %s [%ds]"%(name, time)
+        print "[%s] Played: '%s' [%ds]"%(self.path, name, time)
+        self.Played(name, time)
         index += 1
         if index >=0 and index < len(self.tracks):
             self.PlayTrack(index)
         else:
-            print "End of tracks"
+            print "[%s] End of tracks"%self.path
 
 class MasterAudio(dbus.service.Object):
     interface = bus_name
@@ -79,22 +85,16 @@ class MasterAudio(dbus.service.Object):
         print "Added player '%s' under path: %s"%(name,path)
         return path
 
-def hello():
-    print "Hello world"
-
-DBusGMainLoop(set_as_default=True)
-
-#gobject.timeout_add(100, make_calls)
-val = gobject.timeout_add_seconds(3, hello)
-gobject.source_remove(val)
-
-bus = dbus.SessionBus()
-busname = dbus.service.BusName(bus_name, bus)
-obj = MasterAudio(bus)
-
-loop = gobject.MainLoop()
-loop.run()
+def main():
+    DBusGMainLoop(set_as_default=True)
+    
+    bus = dbus.SessionBus()
+    busname = dbus.service.BusName(bus_name, bus)
+    obj = MasterAudio(bus)
+    
+    loop = gobject.MainLoop()
+    loop.run()
 
 
 if __name__ == '__main__':
-    pass
+    main()
